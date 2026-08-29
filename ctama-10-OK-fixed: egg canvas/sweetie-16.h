34866dde84acf650c84e1ef7fca0aa8c311438ad
rald <thecoconutnutisagiantnut@gmail.com>
2026-08-29 10:26:59 +0800

    OK

---
+++ ctama-10-OK-fixed: egg canvas/sweetie-16.h
@@ -0,0 +1,28 @@
+// sweetie-16.h - STB-style single header include for the Sweetie-16 Palette
+#ifndef SWEETIE_16_H
+#define SWEETIE_16_H
+
+#ifdef SWEETIE_16_IMPLEMENTATION
+
+// Sweetie-16 Palette RGB888 values (GrafxKid) with sweet candy/food color names
+const unsigned int SWEETIE_16_PALETTE[16] = {
+    0x1A1C2C, // 0: Licorice (Dark Blue / Black)
+    0x5D275D, // 1: Blackberry (Dark Purple)
+    0xB13E53, // 2: Cherry Jam (Dark Red)
+    0xEF7D57, // 3: Butterscotch (Orange)
+    0xFFCD75, // 4: Lemon Drop (Yellow)
+    0xA7F070, // 5: Sour Apple (Light Green)
+    0x38B764, // 6: Mint Leaf (Green)
+    0x257179, // 7: Blue Spruce (Dark Teal)
+    0x29366F, // 8: Blueberry Hard-Candy (Indigo)
+    0x3B5DC9, // 9: Bubblegum (Blue)
+    0x41A6F6, // 10: Cotton Candy (Light Blue)
+    0x73EFF7, // 11: Frosting (Cyan)
+    0xF4F4F4, // 12: Powder Sugar (White)
+    0x94B0C2, // 13: Marshmallow Grey (Light Slate Grey)
+    0x566C86, // 14: Truffle (Slate Grey)
+    0x333C57  // 15: Dark Chocolate (Dark Slate)
+};
+
+#endif // SWEETIE_16_IMPLEMENTATION
+#endif // SWEETIE_16_H
